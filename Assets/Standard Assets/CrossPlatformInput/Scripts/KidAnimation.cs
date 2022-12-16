b7bf47528017887b70ac6ce7d9b839bcdf81a4a3
AaditBhatia <aadit.bhatia93@gmail.com>
2022-12-16 13:52:12 -0500

    A bit better animations

--- Assets/Standard Assets/CrossPlatformInput/Scripts/KidAnimation.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/KidAnimation.cs
@@ -28,10 +28,20 @@ public class KidAnimation : MonoBehaviour
         if(isIdle){
             velocity = 0.0f;
         }
-        
+        isIdle = true;
         bool upArrow=Input.GetKey(KeyCode.UpArrow);
         bool downArrow=Input.GetKey(KeyCode.DownArrow);
-        bool space=Input.GetKey(KeyCode.Space);
+        bool shiftPressed=Input.GetKey(KeyCode.LeftShift)||Input.GetKey(KeyCode.RightShift);
+        if(shiftPressed && upArrow){
+            Debug.Log("Should be running");
+            animation_controller.SetBool("isRunning",true);
+            run();
+            isIdle = false;
+        }
+        else{
+            animation_controller.SetBool("isRunning",false);
+
+        }
         if(upArrow){
             animation_controller.SetBool("toWalk",true);
             walk_forward();
@@ -39,17 +49,15 @@ public class KidAnimation : MonoBehaviour
         }
         else{
             animation_controller.SetBool("toWalk",false);
-            isIdle = true;
         }
-        if(space){
-
+        
+        if(downArrow){
             animation_controller.SetBool("toJump",true);
-            jump();
+            walk_backward();
             isIdle = false;
         }
         else{
             animation_controller.SetBool("toJump",false);
-            isIdle = true;
         }
         if(Input.GetKey(KeyCode.LeftArrow)){
                 Debug.Log("bruh");
@@ -80,11 +88,18 @@ public class KidAnimation : MonoBehaviour
         }
         transform.position=new Vector3(transform.position.x+ xdirection*velocity*Time.deltaTime, transform.position.y, transform.position.z+zdirection*velocity*Time.deltaTime);
     }
-    public void jump(){
+    public void jump(){ // Very useless state
         if(velocity<walking_velocity*3.0){
             velocity+=0.5f;
         }
-        transform.position = new Vector3(transform.position.x+ xdirection*velocity*Time.deltaTime, transform.position.y*velocity*Time.deltaTime, transform.position.z+zdirection*velocity*Time.deltaTime);
+        transform.position = new Vector3(transform.position.x+ xdirection*velocity*Time.deltaTime, transform.position.y*velocity*Time.deltaTime*25, transform.position.z+zdirection*velocity*Time.deltaTime);
         //transform.position=new Vector3(transform.position.x+ xdirection*velocity*Time.deltaTime, transform.position.y, transform.position.z+zdirection*velocity*Time.deltaTime);
+    }
+    public void walk_backward(){
+
+        if(velocity>-1*walking_velocity/2.0){
+            velocity-=0.02f;
+        }
+        transform.position=new Vector3(transform.position.x+ xdirection*velocity*Time.deltaTime, transform.position.y, transform.position.z+zdirection*velocity*Time.deltaTime);
     }
 }
